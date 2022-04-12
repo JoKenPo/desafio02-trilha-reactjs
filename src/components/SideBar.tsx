c294f74bea6ff6f9d06fb9c7d5595b6ff8011b64
Eduardo Almeida <duduf2005@hotmail.com>
2022-04-11 22:08:32 -0300

    feat: solução

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,26 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+import { GenreResponseProps } from '../App';
+
+import '../styles/sidebar.scss';
+
+export function SideBar(props: { genres: GenreResponseProps[], selectedGenreId: number, onClick: (id: number) => void }) {
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {props.genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.onClick(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
